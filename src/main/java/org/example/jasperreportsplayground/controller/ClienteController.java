69316418f77809a09277b007b0240399d07af2b5
bescalante <bryant.escalante@sasf.net>
2026-07-30 17:26:02 -0500

    Add frontend files (not functional)

--- src/main/java/org/example/jasperreportsplayground/controller/ClienteController.java
+++ src/main/java/org/example/jasperreportsplayground/controller/ClienteController.java
@@ -21,7 +21,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/clientes")
+@RequestMapping("/comprobantes")
 public class ClienteController {
 
     private final ClienteService clienteService;
